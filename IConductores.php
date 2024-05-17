bd379339d1d71bc7a31b50ff67655ac7b21e7a8c
Joseph <146024442+Josephmx2019@users.noreply.github.com>
2024-05-16 19:37:14 -0600

    Add files via upload

--- IConductores.php
+++ IConductores.php
@@ -2,11 +2,16 @@
     $ID = $_POST['ID'];
     $Nombre = $_POST['Nombre'];
     $Apellidos = $_POST['Apellidos'];
+    $FirmaTmp = file_get_contents($_FILES['Firma']['tmp_name']);
+    $FotoTmp = file_get_contents($_FILES['Foto']['tmp_name']);
     $FechaNacimiento = $_POST['FechaNacimiento'];
     $Genero = $_POST['Genero'];
+    $GrupoSanguineo = $_POST['GrupoSanguineo'];
+    $DonadorOrganos = $_POST['DonadorOrganos'];
     $Telefono = $_POST['Telefono'];
     $Correo = $_POST['Correo'];
     $DireccionID = $_POST['DireccionID'];
+
 
     print("ID Conductores: " . $ID . "<br>");
     print("Nombre: " . $Nombre . "<br>");
@@ -17,7 +22,8 @@
     print("Correo: " . $Correo . "<br>");
     print("Direccion ID: " . $DireccionID . "<br>");
 
-    $sql = "INSERT INTO Conductores (ID, Nombre, Apellidos, FechaNac, Genero, Telefono, Correo, DireccionID) VALUES ('$ID', '$Nombre', '$Apellidos', '$FechaNacimiento', '$Genero', '$Telefono', '$Correo', '$DireccionID')";
+    $sql = "INSERT INTO Conductores (ID, Nombre, Apellidos, Foto, Firma, FechaNac, Genero, GrupoSang, DonadorOrg, Telefono, Correo, DireccionID) VALUES ('$ID', '$Nombre', '$Apellidos', '$FotoTmp', '$FirmaTmp', '$FechaNacimiento', '$Genero', '$GrupoSanguineo','$DonadorOrganos', '$Telefono', '$Correo', '$DireccionID')";
+    
 
 
 include("Controlador.php");
